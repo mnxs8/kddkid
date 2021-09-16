e2a6c4ec4d21e2545440f6faddf4e52e21cb1eb3
جمهورية الهكرات <74466394+mnxs8@users.noreply.github.com>
2021-09-16 20:02:26 +0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-define( 'ID', 'front' );
-
-//define( 'BSMODE', 'util' );
-define( 'BSSYS', TRUE );
-//require_once realpath('').'/app/bsPHP.0.1.php';
-require_once realpath('').'/app/bsPHP.0.2.php';
-?>
